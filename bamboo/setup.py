5347a13efbbfbe16213f743e7e9a2c87ba496664
soasme <soasme@gmail.com>
2016-04-04 16:11:46 +0800

    add dependencies.

--- bamboo/setup.py
+++ bamboo/setup.py
@@ -15,7 +15,11 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    # TODO: put package requirements here
+    'flask',
+    'requests',
+    'PyYAML',
+    'celery',
+    'redis',
 ]
 
 test_requirements = [
