b08f47fd7dbb4785bff03f5a164ea666c5bec3c8
soasme <soasme@gmail.com>
2016-04-04 16:04:13 +0800

    implement pipe api.

--- bamboo/bamboo/application.py
+++ bamboo/bamboo/application.py
@@ -21,7 +21,8 @@ def builtin_jq():
     import subprocess
     program = request.args.get('program', ".")
     data = request.data
-    prog = subprocess.Popen(['jq', program], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    cmd = ['jq', program]
+    prog = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     out, err = prog.communicate(input=data)
     resp = make_response(out)
     resp.content_type = 'application/json'
@@ -43,6 +44,63 @@ def builtin_echo():
 # route: get progs
 # route: get prog run hist
 # route: trigger prog
+
+def _load_program(username, program):
+    import yaml
+    with open('./programs/%s/%s.yml' % (username, program)) as f:
+        data = f.read()
+        data = yaml.load(data)
+        return data
+
+from flask import abort, jsonify
+import os
+from celery import chain
+from celery import Celery
+import requests
+import json
+import logging
+logging.getLogger("urllib3").setLevel(logging.DEBUG)
+
+celery = Celery('app', broker='redis://')
+celery.conf.update(
+    CELERY_RESULT_BACKEND='redis://'
+)
+
+@celery.task
+def prog_runner(input, prog):
+    prog = dict(prog)
+    prog['json'] = input
+    prog.pop('data', None)
+    method = prog.pop('method', 'post').lower()
+    resp = getattr(requests, method)(**prog)
+    if not resp.status_code == 200:
+        abort(400)
+    try:
+        return resp.json()
+    except:
+        return {'_': resp.content}
+
+@app.route('/<username>/<program>', methods=['POST'])
+def run_prog(username, program):
+    detach = request.args.get('detach')
+    program = _load_program(username, program)
+    pipe = program['pipe']
+    tasks = []
+    if not pipe:
+        abort(400)
+    tasks.append(prog_runner.s(request.json, pipe[0]))
+    for prog in program['pipe'][1:]:
+        tasks.append(prog_runner.s(prog))
+    res = chain(*tasks)()
+    if detach:
+        return jsonify(state='triggered')
+    try:
+        data = res.get(timeout=30)
+    except Exception as e:
+        raise e
+    resp = make_response(json.dumps(data))
+    resp.content_type = 'application/json'
+    return resp
 
 # errorhandler: ProgAbort
 
